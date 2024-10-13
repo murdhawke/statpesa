42a5df1caf1bedd751997025fd57ffa66dc40447
murdhawke <amoscheruiyot22@gmail.com>
2024-10-13 19:03:18 +0300

    Loaded db data to a table

--- README.md
+++
@@ -1,2 +0,0 @@
-# statpesa
-M-Pesa Statement Dashboard
---
+++ main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql2');
+const fs = require('fs');
+const cors = require('cors');
+
+const rawData = fs.readFileSync('./config/config.json');
+const config = JSON.parse(rawData);
+
+const dbConfig = {
+    host: 'localhost',
+    user: config.db_username,
+    password: config.db_password
+};
+
+const app = express();
+app.use(express.static('public'));
+const port = 1111;
+app.use(cors());
+
+const db = mysql.createConnection(dbConfig);
+
+app.get('/data', (req, res) => {
+    db.query('SELECT * FROM statpesa.exchange_rates', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
